4a70db24f2c62258823caec8eb6eb359fd3c3645
Jose Cordeiro Andrade <josecordeiro.andrade@Zucchetti.es>
2021-09-02 16:53:01 +0200

    Añadiendo acciones al editor

--- actions.js
+++ actions.js
@@ -8,21 +8,60 @@ for (let item of optionsItem){
 }
 
 const textArea = document.getElementById("editor__body");
+let tagInicial = "";
+let tagFinal = "";
 
 function optionSelected(event) {
-    addOptionSelected();
+    console.log(event.target);
+    let tagSelected = onGetOptionTag(event.target.id);
+    addOptionSelected(tagSelected);
     textArea.focus();
 }
 
-function addOptionSelected(){
+function onGetOptionTag(elementID){
+    switch (elementID){
+        case "h2":
+        case "h3":
+        case "h4":
+            tagInicial = elementID;
+            tagFinal = elementID;
+            break;
+        case "bold":
+            tagInicial = "b"
+            tagFinal = "b";
+            break;
+        case "italic":
+            tagInicial = "i"
+            tagFinal = "i";
+            break;
+        case "underline":
+            tagInicial = "u"
+            tagFinal = "u";
+            break;
+        case "link":
+            let linkValue = prompt("Inserte el link:");
+            let linkName = prompt("Inserte el nombre que se verá:");
+            tagInicial = `a href="${linkValue}">${linkName}`
+            tagFinal = "a";
+            break;
+        case "image":
+            return "img"
+            break;
+        case "video":
+            return "video"
+            break;
+    }
+}
+
+function addOptionSelected( tagSelected ){
     let start = textArea.selectionStart;
     let end = textArea.selectionEnd;
     let textSelected = textArea.value.substring(start, end);
     let originalTextInitial = textArea.value.substring(0, start);
     let originalTextEnd = textArea.value.substring(end);
-    let formatedText = `${originalTextInitial}<b>${textSelected}</b>${originalTextEnd}`;
+    let formatedText = `${originalTextInitial}<${tagInicial}>${textSelected}</${tagFinal}>${originalTextEnd}`;
     textArea.value = formatedText;
-    textArea.setSelectionRange(start + 3, end + 3);
+    textArea.setSelectionRange(start + 2 + tagInicial.length, end + 2 + tagFinal.length);
 }
 
 function onClear(){
